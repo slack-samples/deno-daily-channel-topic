88c37a34f92154f07ba09dd534776ea3b9616328
Fil Maj <fmaj@slack-corp.com>
2023-05-10 11:18:26 -0400

    Use trigger data and type enumerations in trigger definition. (#11)

--- triggers/create_schedule_shortcut.ts
+++ triggers/create_schedule_shortcut.ts
@@ -1,13 +1,15 @@
 import { Trigger } from "deno-slack-sdk/types.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
+import { CreateScheduleWorkflow } from "../workflows/create_schedule.ts";
 
-const trigger: Trigger = {
-  type: "shortcut",
+const trigger: Trigger<typeof CreateScheduleWorkflow.definition> = {
+  type: TriggerTypes.Shortcut,
   name: "Create Schedule",
   description: "Schedule the Daily update of a channel topic",
-  workflow: "#/workflows/create_schedule",
+  workflow: `#/workflows/${CreateScheduleWorkflow.definition.callback_id}`,
   inputs: {
     interactivity: {
-      value: "{{data.interactivity}}",
+      value: TriggerContextData.Shortcut.interactivity,
     },
   },
 };
